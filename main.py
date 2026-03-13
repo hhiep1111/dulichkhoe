19d44492902988c5f3be1b2cf895a0cf72964b7f
hhiep1111 <hiepphce180344@fpt.edu.vn>
2026-03-13 23:36:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2674,7 +2674,7 @@ Quy tắc:
     try:
 
         response = client.models.generate_content(
-            model="gemini-2.0-flash",
+            model="gemini-2-flash",
             contents=prompt
 		)
 
